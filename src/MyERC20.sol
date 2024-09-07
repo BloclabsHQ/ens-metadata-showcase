ed38eead8403a5f47a7a1b4e64262376578c07c8
madgeniusblink <madgeniusblink@gmail.com>
2024-09-06 20:05:20 -0700

    first ENSMetadata implementation in a ERC20 contract

--- src/MyERC20.sol
+++ src/MyERC20.sol
@@ -2,10 +2,22 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "ENSMetadataStandard/ENSMetadata.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ENSMetadata} from "ENSMetadataStandard/ENSMetadata.sol";
 
-contract MyToken is ERC20, ERC20Permit {
-    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
+contract MyToken is ERC20, ERC20Permit, ENSMetadata {
+    constructor()
+        ERC20("MyToken", "MTK")
+        ERC20Permit("MyToken")
+        ENSMetadata(
+            "The World Token Project",
+            "This token is for the world token project",
+            "worldtokenproject.eth"
+        )
+    {}
+    // Function to trigger ENS verification
+    function triggerENSVerification() public onlyOwner returns (bool) {
+        return verifyENS();
+    }
 }
