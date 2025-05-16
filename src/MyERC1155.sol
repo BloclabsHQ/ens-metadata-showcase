bbc4ce3f0e18345dad25b91e631ed3b11854ea69
adnan1aust <adnan1aust@users.noreply.github.com>
2025-05-16 23:05:25 +0600

    FEAT: Showcase update (#4)

--- src/MyERC1155.sol
+++ src/MyERC1155.sol
@@ -4,9 +4,21 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import {ENSMetadata} from "ENSMetadataStandard/ENSMetadata.sol";
 
-contract MyToken is ERC1155, Ownable {
-    constructor(address initialOwner) ERC1155("https://example.com") Ownable(initialOwner) {}
+contract MyToken is ERC1155, Ownable, ENSMetadata {
+    constructor(address initialOwner, address _ensRegistry) 
+        ERC1155("https://example.com") 
+        Ownable(initialOwner)
+        ENSMetadata(
+            "ENS Metadata ERC1155",
+            "An example ERC1155 contract showcasing ENS metadata integration",
+            "ensmetadata1155.eth",
+            _ensRegistry,
+            new string[](0), // Empty array for social media links
+            ""  // Empty string for external data URI
+        )
+    {}
 
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
